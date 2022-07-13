4e61c1a03552e135b39d1336e3d6ba4de2da78a6
fayzawina <108259646+fayzawina@users.noreply.github.com>
2022-07-13 11:30:00 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,17 @@
-
 #include "main.h"
-
 /**
-* _isupper -> this is a function to check upper case alphas
-* @c: an integer paramer
-* Return: something but success
+* _isupper - checks if parameter is an uppercase character.
+* @c: input character.
+* Return: 1 if is an uppercase character, 0 in other case.
 */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
